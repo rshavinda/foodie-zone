28ddfe5cecfbcddf8a6179bc5f97259a4094956a
Rajitha Shavinda <rajitha.shavinda@gmail.com>
2022-12-30 13:26:50 +0530

    Init home screen

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{Component} from 'react';
+import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import Header from './src/components/Header';
+import { colors } from './src/global/styles';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor={colors.statusbar}
+      />
+      <Header titleVal= "Home"/>
+
     </View>
-  );
+  )
 }
 
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  container: {flex:1}
+})
---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React,  {Component} from "react";
+import { View,Text, StyleSheet, Dimensions } from "react-native";
+import {colors, parameters} from "../global/styles";
+import { Icon } from 'react-native-elements';
+
+
+export default function Header({titleVal}) {
+
+    return (
+        <View style={styles.header}>
+
+            <View style={{marginLeft:20}}>
+                <Icon
+                    type="material-community"
+                    name="arrow-left"
+                    color={colors.headerText}
+                    size={28}
+                    onPress={() => { }}
+                />
+
+            </View>
+
+            <View>
+                <Text style={styles.headerText}> {titleVal} </Text>
+            </View>
+
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    header :{
+        flexDirection:"row",
+        backgroundColor:colors.buttons,
+        height:parameters.headerHeight,
+    },
+
+    headerText:{
+        color:colors.headerText,
+        fontSize:22,
+        fontWeight:'bold',
+        marginLeft:30,
+    }
+})
